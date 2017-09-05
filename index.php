0c420ac4c2d54e45e60c8115b6fe6527b26b6d65
mjperales <m.j.perales@tcu.edu>
2017-09-05 13:12:52 -0500

    updated class names to have mjp prefix

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 $pagetitle = 'Current Work';
 include 'header.php';
 ?>
-<div class="tcu-layoutwrap--transparent">
+<div class="mjp-layoutwrap--transparent">
 
-    <div class="tcu-layout-constrain inner-content cf">
+    <div class="mjp-layout-constrain inner-content cf">
 
         <?php
         /*
@@ -12,7 +12,7 @@ include 'header.php';
          * main navigation link for accessibility
          */
         ?>
-        <main class="unit size1of1 m-size1of1 tcu-below16 cf" id="main">
+        <main class="unit size1of1 m-size1of1 mjp-below16 cf" id="main">
         <h1>HTML Ipsum Presents</h1>
 
         <p><strong>Pellentesque habitant morbi tristique</strong> senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. <em>Aenean ultricies mi vitae est.</em> Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, <code>commodo vitae</code>, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. <a href="#">Donec non enim</a> in turpis pulvinar facilisis. Ut felis.</p>
@@ -55,7 +55,7 @@ include 'header.php';
 
         </main><!-- end of .unit -->
 
-    </div><!-- end of .tcu-layout-constrain -->
+    </div><!-- end of .mjp-layout-constrain -->
 
-</div><!-- end .tcu-layoutwrap--transparent -->
+</div><!-- end .mjp-layoutwrap--transparent -->
 <?php  include 'footer.php'; ?>
